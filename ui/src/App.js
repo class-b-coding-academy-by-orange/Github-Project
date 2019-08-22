20a0d1ac36956e3beaed5f069a49ca68ae8f9746
sqoor <alsqoorAbdullah@gmail.com>
2019-08-22 16:15:29 +0300

    added  server and the front-end

---
+++ ui/src/App.js
@@ -0,0 +1,64 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+import Search from "./components/Search";
+import Table from "./components/Table";
+
+class App extends Component {
+  state = {
+    repos: [],
+    userFound: false,
+    searchedForUser: false
+  };
+
+  getRepos = async usernameToSearchFor => {
+    try {
+      const res = await axios.get(
+        `http://localhost:9000/repos/${usernameToSearchFor}`
+      );
+      console.log(res.data);
+      if (res.data === "error") {
+        this.setState({
+          repos: [],
+          searchedForUser: false,
+          userFound: false
+        });
+      } else if (res.data.length === 0)
+        this.setState({
+          repos: [],
+          searchedForUser: true,
+          userFound: false
+        });
+      else if (res.data.length > 0)
+        this.setState({
+          repos: res.data,
+          searchedForUser: true,
+          userFound: true
+        });
+    } catch (err) {
+      console.log(err);
+      this.setState({ repos: [], searchedForUser: true, userFound: false });
+    }
+  };
+
+  renderReposOrMessage = () => {
+    const { userFound, searchedForUser } = this.state;
+
+    if (!searchedForUser) return "";
+    else if (userFound) return <Table repos={this.state.repos} />;
+    else return <h3 className="m-5">User is not found!</h3>;
+  };
+
+  render() {
+    const { getRepos, renderReposOrMessage } = this;
+
+    return (
+      <div className="container m-3">
+        <Search getRepos={getRepos} />
+        {renderReposOrMessage()}
+      </div>
+    );
+  }
+}
+
+export default App;
