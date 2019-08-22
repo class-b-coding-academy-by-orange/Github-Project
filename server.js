ce3744e309adc9ff35d004ded58c55c1717f3de7
orayb-alsmadi <oraybalsmadi@yahoo.com>
2019-08-22 15:23:18 +0300

    project folders structure

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req, res) => {
-  res.json('server is working');
+app.get("/", (req, res) => {
+  res.json("server is working");
 });
 
 /*
@@ -17,15 +17,64 @@ app.get('/tasks', (req, res) => {
     res.json(result);
   })
 });
+
+************************************************************
+
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const logger = require("morgan");
+const axios = require("axios");
+
+app.use(express.json());
+app.use(logger("dev"));
+app.use(cors());
+
+// const playersRouter = require("./routes/players");
+// app.use("/players", playersRouter);
+
+app.get("/:s", (req, res) => {
+  let s = req.params.s;
+  let request = `https://api.github.com/users/${s}/repos`;
+  axios
+    .get(request)
+    .then(response => {
+      res.send(response.data.slice(0, 5));
+    })
+    .catch(error => {
+      res.send("User is not Found");
+    });
+});
+
+app.post("/data", (req, res) => {
+  let newData = req.body;
+  console.log(newData);
+  data.push(newData);
+  res.json(data);
+});
+
+data = [
+  {
+    id: 1,
+    name: "Ahmad",
+    Age: 27
+  },
+  {
+    id: 2,
+    name: "Orayb",
+    Age: 27
+  }
+];
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log("it's listening");
+});
+
+module.exports = app;
 */
 
 // Start your code below
 
-
-
-
-
-
-
-const PORT = process.env.PORT || 9000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`Server listening to ${PORT}`));
