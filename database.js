d3c6f2423eece04ba61ef09eb40a76d3d5831aef
emad-alrsheed <alrash.emad@gmail.com>
2019-08-26 00:29:53 +0300

    finsh

--- database.js
+++ database.js
@@ -43,4 +43,34 @@ module.exports = {
 
 // Start your code below
 
+const reposSchema = new mongoose.Schema({
+  title: String,
+  language: String,
+  state: Boolean,
+});
 
+const Repos = mongoose.model('repos', reposSchema);
+
+const getRepos = async () => {
+  return await Repos.find({});
+} 
+
+const addRepo = async (newRepo) => {
+  return await Repos.create(newRepo);
+}
+
+const updateRepo = async (_id, newRepoContent) => {
+  return await Repos.updateOne({_id}, {$set: newRepoContent})
+}
+
+const deleteRepo = async (_id) => {
+  return await Repos.deleteOne({_id});
+}
+
+
+module.exports = {
+  getRepos,
+  addRepo,
+  updateRepo,
+  deleteRepo
+}
---
+++ my-app/src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+// import Search from './components/Search';
+import Table from './components/Table';
+
+class App extends Component {
+  state = {
+    repos: []
+  }
+
+  getUsername = (username) => {
+    axios.get(`https://api.github.com/users/${username}/repos`)
+      .then(res => {
+        this.setState({ repos: res.data })
+      });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {/* <Search ahamd={'emad'} getUsername={this.getUsername} /> */}
+        <Table data={this.state.repos} />
+    </div>
+    );
+  }
+}
+
+export default App;
