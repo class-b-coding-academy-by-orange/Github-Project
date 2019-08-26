871b645e91e68f767f894a5eb5a6f6b7e3542fd5
alaarg <ahmadalaarg123@gmail.com>
2019-08-26 14:59:46 +0300

    finished the exercise

--- database.js
+++ database.js
@@ -1,16 +1,16 @@
-const mongoose = require('mongoose');
+  const mongoose = require("mongoose");
 
-mongoose.connect('mongodb://localhost/Repos', { useNewUrlParser: true });
+mongoose.connect("mongodb://localhost/Repos", { useNewUrlParser: true });
 const db = mongoose.connection;
 
-db.on('error', function () {
-  console.log('mongoose connection error');
-  console.log('____________________________')
+db.on("error", function() {
+  console.log("mongoose connection error");
+  console.log("____________________________");
 });
 
-db.once('open', function () {
-  console.log('mongoose connected successfully');
-  console.log('____________________________')
+db.once("open", function() {
+  console.log("mongoose connected successfully");
+  console.log("____________________________");
 });
 
 /*
@@ -43,4 +43,57 @@ module.exports = {
 
 // Start your code below
 
+const tasksSchema = new mongoose.Schema({
+  title: String,
+  language: String,
+  status: Boolean
+});
 
+let Repos = new mongoose.model("repos", tasksSchema);
+
+let getRepos = cb => {
+  Repos.find({}, (err, data) => {
+    if (err) {
+      cb(err);
+    } else {
+      cb(data);
+    }
+  });
+};
+
+let addRepo = (repo, cb) => {
+  Repos.create(repo, (err, data) => {
+    if (err) {
+      cb(err);
+    } else {
+      cb(data);
+    }
+  });
+};
+
+let updateRepo = (id, updatedStatus, cb) => {
+  Repos.updateOne({_id: id}, { $set: updatedStatus }, (err, data) => {
+    if (err) {
+      cb(err);
+    } else {
+      cb(data);
+    }
+  });
+};
+
+let deleteRepo = (id, cb) => {
+  Repos.deleteOne({ _id: id }, (err, data) => {
+    if (err) {
+      cb(err);
+    } else {
+      cb(data);
+    }
+  });
+};
+
+module.exports = {
+  addRepo,
+  getRepos,
+  updateRepo,
+  deleteRepo
+};
